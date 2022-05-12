7b9e8e3d930ca75c1283b06c0a3d4251be34de39
Ding Xue Wing <55356736+xuewing@users.noreply.github.com>
2022-05-12 10:58:38 +0800

    Add files via upload

---
+++ Car Rental Management System/Admin.h
@@ -0,0 +1,24 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+class Admin
+{
+public:
+	void loginAdmin();
+	void adminOptions();
+	void addNewAdmin();
+	void admin();
+	void addNewCar();
+	void removeCar();
+	void addNewUser();
+	void addNewBooking();
+	void removeBooking();
+	void showAllUser();
+	void findBooking();
+	void editUserDetail();
+	void trackPayment();
+	void viewFeedback();
+	void generateStatistic();
+
+};
+#endif
